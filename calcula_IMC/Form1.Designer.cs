7be4838adf8d61dbde90acc7f282a4dd023fcb51
PATRICK SANTOS TRINDADE <rs108954@gmail.com>
2023-02-05 13:10:48 -0300

    Adicionar arquivos de projeto.

---
+++ calcula_IMC/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace calcula_IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.lbllegal = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(130, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(130, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PESO";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(280, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ALTURA";
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(280, 172);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(56, 15);
+            this.lblresultado.TabIndex = 5;
+            this.lblresultado.Text = "resultado";
+            this.lblresultado.Visible = false;
+            this.lblresultado.Click += new System.EventHandler(this.lblresultado_Click);
+            // 
+            // lbllegal
+            // 
+            this.lbllegal.AutoSize = true;
+            this.lbllegal.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbllegal.ForeColor = System.Drawing.Color.White;
+            this.lbllegal.Location = new System.Drawing.Point(280, 218);
+            this.lbllegal.Name = "lbllegal";
+            this.lbllegal.Size = new System.Drawing.Size(38, 15);
+            this.lbllegal.TabIndex = 6;
+            this.lbllegal.Text = "label4";
+            this.lbllegal.Visible = false;
+            this.lbllegal.Click += new System.EventHandler(this.lbllegal_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Enabled = false;
+            this.btnCalcular.Location = new System.Drawing.Point(464, 116);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(82, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lbllegal);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Label lbllegal;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ calcula_IMC/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcula_IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void lblresultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double imc = 0, num1, num2;
+            try
+            {
+                num1 = Convert.ToDouble(textBox1.Text);
+                num2 = Convert.ToDouble(textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBox1.Clear();
+                textBox2.Clear();
+                return;
+            }
+           
+
+            imc = num1 / (num2 * num2);
+            if (imc <= 18.50)
+            {
+                lbllegal.Text = ("ABAIXO DO PESO");
+                lbllegal.ForeColor = Color.Red;
+            }
+            else if (imc >= 18.51 && imc <= 24.90)
+            {
+                lbllegal.Text = ("PESO IDEAL PARABÉNS");
+            }
+            else if (imc >= 24.91 && imc <= 29.90)
+            {
+                lbllegal.Text = ("LEVEMENTE ACIMA DO PESO");
+            }
+            else if (imc >= 29.91 && imc <= 34.90)
+            {
+                lbllegal.Text = ("OOBESIDADE GRAU 1");
+            }
+            else if (imc >= 34.91 && imc <= 39.90)
+            {
+                lbllegal.Text = ("OOBESIDADE GRAU 2(SEVERA)");
+            }
+            else if (imc >= 39.91)
+            {
+                lbllegal.Text = ("OOBESIDADE GRAU 3 (MÓRBIDA)");
+            }
+            lblresultado.Text = imc.ToString("n");
+            lblresultado.Visible = true;
+            lbllegal.Visible = true;
+        }
+
+        private void lbllegal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox2.Text == "" || textBox1.Text == "")
+            {
+                btnCalcular.Enabled = false;
+                lbllegal.Visible = false;
+                lblresultado.Visible = false;
+            }
+            else
+            {
+                btnCalcular.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "" || textBox1.Text == "")
+            {
+                btnCalcular.Enabled = false;
+                lbllegal.Visible = false;
+                lblresultado.Visible = false;
+            }
+            else
+            {
+                btnCalcular.Enabled = true;
+            }
+        }
+    }
+    
+}
